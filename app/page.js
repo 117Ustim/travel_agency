81f6e4c03bdc8af76eb25723b150af53dd7cc41a
117Ustim <ustik72@gmail.com>
2025-10-21 14:19:25 +0300

    17

--- app/page.js
+++ app/page.js
@@ -13,15 +13,15 @@ import Footer from "./comp/footer/Footer";
 export default function Home() {
   return (
     <div className={styles.page}>
-     {/* <Main />
+     <Main />
      <Section1/>
-     <Section2/> */}
+     <Section2/>
        <Section3/>  
     <Section4/>
       <Section5/> 
       <Section6/>
     <Section7/>
-      {/*<Footer/> */}
+      <Footer/>
     </div>
   );
 }
